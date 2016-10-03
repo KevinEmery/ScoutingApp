239fed483ae5cddb9e05b1bda6dd73d85e882654
Anne Gwynne-Robson <angwy@microsoft.com>
2016-10-02 19:17:05 -0700

    Able to query if external storage is writable and generate a toast with the result of this query.

--- app/src/main/java/com/example/angwy/scoutingapp/MainActivity.java
+++ app/src/main/java/com/example/angwy/scoutingapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.angwy.scoutingapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void SaveDataToFile(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "";
+        int duration = Toast.LENGTH_SHORT;
+
+        if (this.isExternalStorageWritable())
+        {
+            text = "Can write to storage";
+        }
+        else
+        {
+            text = "Unable to save data to file.";
+        }
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
