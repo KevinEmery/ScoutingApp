3c5370ef136a618c89cf9c91d9851b0527f00a80
Anne Gwynne-Robson <angwy@microsoft.com>
2016-10-02 19:58:36 -0700

    Added write to file code but am getting 'drectory not created' errors. Suspect it's because I'm trying to write something that already exists.

--- app/src/main/java/com/example/angwy/scoutingapp/MainActivity.java
+++ app/src/main/java/com/example/angwy/scoutingapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.angwy.scoutingapp;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +24,28 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void SaveDataToFile(View view) {
         Context context = getApplicationContext();
-        CharSequence text = "";
+        CharSequence text = "null";
         int duration = Toast.LENGTH_SHORT;
 
         if (this.isExternalStorageWritable())
         {
-            text = "Can write to storage";
+            String content = "hello world";
+
+            try {
+                File dataFileHandle = this.getDocumentStorageDir("ScoutingData");
+                FileOutputStream outputStream = new FileOutputStream(dataFileHandle);
+                outputStream.write(content.getBytes());
+                outputStream.close();
+
+                text = getString(R.string.data_written) + dataFileHandle.getPath();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, e.toString());
+                text = getString(R.string.unable_to_write_file);
+            }
         }
         else
         {
-            text = "Unable to save data to file.";
+            text = getString(R.string.unable_to_write_file);
         }
 
         Toast toast = Toast.makeText(context, text, duration);
@@ -42,5 +59,17 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    public File getDocumentStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), albumName);
+        if(!file.exists()) {
+            if (!file.mkdirs()) {
+                Log.e(LOG_TAG, "Directory not created");
+            }
+        }
+        return file;
     }
 }
