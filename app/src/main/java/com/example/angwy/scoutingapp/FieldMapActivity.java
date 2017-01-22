ab88523e1ce214b16ffa327db80abcf0f3c8de8d
Anne Gwynne-Robson <agwynnerobson@gmail.com>
2017-01-22 15:08:51 -0800

    Added a new activity for the field layout as well as some pics of the field to use while testing

---
+++ app/src/main/java/com/example/angwy/scoutingapp/FieldMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.angwy.scoutingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FieldMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_field_map);
+    }
+}
