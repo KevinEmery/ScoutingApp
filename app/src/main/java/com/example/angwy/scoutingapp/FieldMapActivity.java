1d34044baf87e49ad19e2f962a860d2e975cfb9e
Anne Gwynne-Robson <agwynnerobson@gmail.com>
2017-01-22 17:43:13 -0800

    Ditched the circle menu thing because we can accomplish the same thing with simpler views

--- app/src/main/java/com/example/angwy/scoutingapp/FieldMapActivity.java
+++ app/src/main/java/com/example/angwy/scoutingapp/FieldMapActivity.java
@@ -1,7 +1,10 @@
 package com.example.angwy.scoutingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
 public class FieldMapActivity extends BaseActivity {
 
@@ -9,5 +12,18 @@ public class FieldMapActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_map);
+
+        View view = findViewById(R.id.activity_field_map);
+        view.setOnTouchListener(handleTouch);
     }
+
+    private OnTouchListener handleTouch = new OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Intent intent = new Intent(FieldMapActivity.this, TeleOpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+    };
 }
