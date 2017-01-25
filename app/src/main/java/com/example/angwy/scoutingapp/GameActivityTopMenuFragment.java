88d71233a47ced6b0f09d5e0795062c278422965
Anne Gwynne-Robson <agwynnerobson@gmail.com>
2017-01-24 17:43:21 -0800

    Actually have a semi-working draft interface (!).

--- app/src/main/java/com/example/angwy/scoutingapp/GameActivityTopMenuFragment.java
+++ app/src/main/java/com/example/angwy/scoutingapp/GameActivityTopMenuFragment.java
@@ -1,37 +1,23 @@
 package com.example.angwy.scoutingapp;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link GameActvityTopMenuFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link GameActvityTopMenuFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Fragment that contains the first menu that shows when you push the field button.
  */
 public class GameActivityTopMenuFragment extends Fragment {
+
+    Button buttonClimbing;
+    Button buttonShooting;
+
     public GameActivityTopMenuFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment GameActvityTopMenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GameActivityTopMenuFragment newInstance() {
-        GameActivityTopMenuFragment fragment = new GameActivityTopMenuFragment();
-        return fragment;
     }
 
     @Override
@@ -40,9 +26,38 @@ public class GameActivityTopMenuFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_actvity_top_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_game_actvity_top_menu, container, false);
+        addButtonClickListeners(view);
+        return view;
+    }
+
+    public void addButtonClickListeners(View view) {
+
+        buttonClimbing = (Button) view.findViewById(R.id.buttonClimbing);
+        buttonShooting = (Button) view.findViewById(R.id.buttonShooting);
+
+        buttonClimbing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClimbingFragment newFragment = new ClimbingFragment();
+                FragmentChangeListener fragmentChangeListener = (FragmentChangeListener) getActivity();
+                fragmentChangeListener.replaceFragment(newFragment);
+            }
+        });
+
+        buttonShooting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create fragment and give it an argument specifying the article it should show
+                ShootingFragment newFragment = new ShootingFragment();
+                FragmentChangeListener fragmentChangeListener = (FragmentChangeListener) getActivity();
+                fragmentChangeListener.replaceFragment(newFragment);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/angwy/scoutingapp/RecordGameEventActivity.java
+++ app/src/main/java/com/example/angwy/scoutingapp/RecordGameEventActivity.java
@@ -1,17 +1,16 @@
 package com.example.angwy.scoutingapp;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordGameEventActivity extends BaseActivity implements FragmentChangeListener {
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    Button buttonBackToField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +21,32 @@ public class RecordGameEventActivity extends BaseActivity implements FragmentCha
         firstFragment.setArguments(getIntent().getExtras());
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, firstFragment).commit();
+        addButtonClickListeners();
     }
 
     /**
      * Helps with switching from fragment to fragment
-     * @param fragment
+     * @param fragment The fragment to replace the current fragment with.
      */
     @Override
     public void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();;
+        FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, fragment, fragment.toString());
         fragmentTransaction.addToBackStack(fragment.toString());
         fragmentTransaction.commit();
+    }
+
+    public void addButtonClickListeners() {
+        buttonBackToField = (Button) findViewById(R.id.buttonReturnToField);
+
+        buttonBackToField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to the field activity
+                Intent intent = new Intent(RecordGameEventActivity.this, FieldMapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
