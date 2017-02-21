f4eb586c6d1aa5b07879f40ec5e3482af81c8cc7
Johan Sundstrom <johansu@msn.com>
2017-02-20 23:27:33 -0800

    Fixed issues with Setup Activity

--- app/src/main/java/org/usfirst/frc/team4911/scouting/SetupActivity.java
+++ app/src/main/java/org/usfirst/frc/team4911/scouting/SetupActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,13 +13,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by johansu on 2/20/2017.
  */
 
 public class SetupActivity extends AppCompatActivity {
 
-    EditText edit_eventCode;
+    Spinner spinner_eventName;
     EditText edit_scoutName;
     EditText edit_scoutTeam;
     Spinner spinner_driveStation;
@@ -37,48 +41,72 @@ public class SetupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
-        edit_eventCode = (EditText)findViewById(R.id.editText_EventCode);
+        spinner_eventName = (Spinner)findViewById(R.id.spinner_event_name);
         edit_scoutName = (EditText)findViewById(R.id.editText_ScoutName);
+
+        // ToDo: consider making team selection a spinner or radio group
         edit_scoutTeam = (EditText)findViewById(R.id.editText_ScoutTeam);
         spinner_driveStation = (Spinner)findViewById(R.id.spinner_drive_station);
         button_savePreferences = (Button)findViewById(R.id.button_Save_Preverences);
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
-        edit_eventCode.setText(sharedpreferences.getString(EventCode, ""));
+        Resources res = getResources();
+        final String[] event_codes = res.getStringArray(R.array.pnw_eventcodes_array);
+        final String[] drive_stations = res.getStringArray(R.array.drive_stations_array);
+
+        String event_Name = (sharedpreferences.getString(EventCode, ""));
         edit_scoutName.setText(sharedpreferences.getString(ScoutName, ""));
         edit_scoutTeam.setText(sharedpreferences.getString(ScoutTeam, ""));
         String drive_Station = sharedpreferences.getString(DriveStation, "");
 
-        Resources res = getResources();
-        String[] drive_stations = res.getStringArray(R.array.drive_stations_array);
-        int position;
-        for (position = 0; position < drive_stations.length; position++) {
-            if (drive_stations[position].equals(drive_Station)) {
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> eventNameAdapter = ArrayAdapter.createFromResource(this,
+                R.array.pnw_events_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        eventNameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> driveStationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.drive_stations_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        driveStationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the selection
+        spinner_eventName.setAdapter(eventNameAdapter);
+        for (int i = 0; i < event_codes.length; i++) {
+            if (event_codes[i].equals(event_Name)) {
+                spinner_eventName.setSelection(i % event_codes.length);
                 break;
             }
         }
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.drive_stations_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        if (position < drive_stations.length)
-        {
-            adapter.setDropDownViewResource(position);
-        }
+        spinner_driveStation.setAdapter(driveStationAdapter);
 
-        // Apply the adapter to the spinner
-        spinner_driveStation.setAdapter(adapter);
+        for (int position = 0; position < drive_stations.length; position++) {
+            if (drive_stations[position].equals(drive_Station)) {
+                spinner_driveStation.setSelection(position % drive_stations.length);
+                break;
+            }
+        }
 
         button_savePreferences.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String event_Code  = edit_eventCode.getText().toString();
+                String event_Code = event_codes[spinner_eventName.getSelectedItemPosition()];
                 String scout_Name = edit_scoutName.getText().toString();
-                String scout_Team  = edit_scoutTeam.getText().toString();
+                String scout_Team = edit_scoutTeam.getText().toString();
                 String drive_station = (String) spinner_driveStation.getItemAtPosition(spinner_driveStation.getSelectedItemPosition());
+
+                boolean isFinished = scout_Name.length() > 2 && scout_Team.length() > 2;
+
+                if (!isFinished)
+                {
+                    if (scout_Name.length() > 2 && scout_Team.length() > 2)
+                    {
+                        // TODO: Alert User with dialog
+                    }
+                }
 
                 SharedPreferences.Editor editor = sharedpreferences.edit();
                 editor.putString(EventCode, event_Code);
@@ -87,7 +115,9 @@ public class SetupActivity extends AppCompatActivity {
                 editor.putString(DriveStation, drive_station);
                 editor.commit();
 
-                Toast.makeText(SetupActivity.this,"Thanks",Toast.LENGTH_LONG).show();
+                //Toast.makeText(SetupActivity.this, "Thanks", Toast.LENGTH_LONG).show();
+
+                finish();
             }
         });
     }
