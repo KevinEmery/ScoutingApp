b44ab1f540ec47d875f4f82489ccb1f1ce2a255b
Johan Sundstrom <johansu@msn.com>
2017-03-03 09:55:53 -0800

    Fixed hopper location validation and setting attempt values

--- app/src/main/java/org/usfirst/frc/team4911/scouting/HopperAttemptFragment.java
+++ app/src/main/java/org/usfirst/frc/team4911/scouting/HopperAttemptFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.util.Pair;
 import android.view.Gravity;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -98,9 +99,7 @@ public class HopperAttemptFragment extends Fragment implements
             message = "Activated hopper: " + hopperPosition;
         }
 
-        Toast toast = Toast.makeText(getContext(), message, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
-        toast.show();
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     /**
